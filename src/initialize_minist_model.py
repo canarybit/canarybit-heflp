48791d8971f619421d6b438502faa3b705fe9d27
yijing <hejdesperado@gmail.com>
2024-09-08 17:34:35 +0200

    test shape

--- src/initialize_minist_model.py
+++ src/initialize_minist_model.py
@@ -1,5 +1,8 @@
 from models import FCN
+import numpy as np
 from heflp.training.params import save_flattened_model_params
 
 model = FCN()
 save_flattened_model_params("fcn_init.npy", model)
+array = np.load('fcn_init.npy')
+print(array.shape)
