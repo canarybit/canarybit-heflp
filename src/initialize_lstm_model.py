a78c7992d78f956123ba86be913251cff1194bf1
yijing <hejdesperado@gmail.com>
2024-10-17 19:58:29 +0200

    added initial x_train data

--- src/initialize_lstm_model.py
+++ src/initialize_lstm_model.py
@@ -6,10 +6,86 @@ from tensorflow.keras.layers import Input, Masking, Dense, Dropout, LSTM, Repeat
 
 import sys
 import pandas as pd
+timesteps_max = 10
+
+def timesteps_calculation(df, timesteps_max):
+
+    timesteps_calc = int(pd.Series(df.ProcessGuid).value_counts().quantile(0.5))
+    if timesteps_calc >= timesteps_max:
+        timesteps = timesteps_max
+    else:
+        timesteps = timesteps_calc
+
+    print("timesteps:", timesteps)
+
+    return timesteps
+
+def reshaping_data(X, timesteps, test):
+    
+    """
+    Reshapes the input data into a suitable format for LSTM models.
+    
+    Args:
+        X (ndarray): Input data array of shape (n_samples, features).
+        timesteps (int): Number of time steps or sequence length for the reshaped data.
+        test (bool): Specify whether to reshape the tet or training dataset. If test=True it will be reshaped, otherwise train.
+        
+    Returns:
+
+        ndarray: Reshaped input data array of shape (n_samples - timesteps + 1, timesteps, features).
+    
+    """
+    
+    if test:
+        Xs = pd.DataFrame()
+        for i in X.ProcessGuid.unique():
+            sorteddf=X[X.ProcessGuid == i].sort_values(by="UtcTime")
+            temp_col = sorteddf["UtcTime"]
+            Attack = sorteddf["Attack"]
+            matrix_temporal = sorteddf.drop(["ProcessGuid","UtcTime","Attack","collector_node_id"], axis=1).values # "collector-node-id"
+
+            # Define padding values and amount
+            padding_value = -1  # Change this to the value you want for padding
+            padding_rows = timesteps - 1  # Number of rows to add as padding
+
+            # Pad the matrix along the rows
+            padded_matrix = np.pad(matrix_temporal, ((padding_rows, 0), (0, 0)), mode='constant', constant_values=padding_value)
+
+            for j,z1,z2 in zip(range(len(matrix_temporal) - timesteps + 1),temp_col,Attack): # Ensures that extracted substrings have uniform length of timesteps and do not go outside the original sequence boundary. Avoid extracting incomplete substring
+                data = {'Value': [padded_matrix[j:(j + timesteps)]],
+                        'ProcessGuid': [i],
+                        'UtcTime':z1,
+                        "numVentana":j,
+                        "Attack":z2}
+                df = pd.DataFrame(data)
+                Xs=pd.concat([Xs,df])
+        return Xs
+    else:
+        Xs = []
+        for i in X.ProcessGuid.unique():
+            matrix_temporal = X[X.ProcessGuid == i].sort_values(by="UtcTime").drop(["ProcessGuid","UtcTime"], axis=1).values
+
+            # Define padding values and amount
+            padding_value = -1  # Change this to the value you want for padding
+            padding_rows = timesteps - 1  # Number of rows to add as padding
+
+            # Pad the matrix along the rows
+            padded_matrix = np.pad(matrix_temporal, ((padding_rows, 0), (0, 0)), mode='constant', constant_values=padding_value)
+
+            for j in range(len(matrix_temporal) - timesteps + 1): # Ensures that extracted substrings have uniform length of timesteps and do not go outside the original sequence boundary. Avoid extracting incomplete substring
+                Xs.append(padded_matrix[j:(j + timesteps)])
+
+        return np.array(Xs)
+
+
 
 input_file = sys.argv[1]
-df = pd.read_csv(input_file)
-input_shape = (df.shape[1], df.shape[2])
+X_train = pd.read_csv(input_file)
+
+timesteps = timesteps_calculation(X_train, timesteps_max)
+X_train = reshaping_data(X_train, timesteps=timesteps,test=False)
+
+input_shape = (X_train.shape[1], X_train.shape[2])
 initial_seq = Sequential()
 initial_seq.add(Input(shape=input_shape))
 initial_seq.add(Masking(mask_value=-1)) # Must match padding_value 
