f41891cca4e2dbc472d245f11b7e0033b104294a
yijing <hejdesperado@gmail.com>
2024-11-03 15:00:04 +0100

    no split data in runner

--- src/fl-client-ti.py
+++ src/fl-client-ti.py
@@ -41,13 +41,13 @@ def load_data(cid:int=0, n_splits:int=1, timesteps:int=10):
     df_test = reading_files(os.path.join(input_data_folder_path,"X_test.csv"))
 
     cols = list(X_test.columns)
-    if n_splits!=1:
-        try:
-            splits = split_training_dataset(X_train, n_splits)
-            X_train = splits[cid]
-        except:
-            print("Split data failed!")
-            exit()
+    # if n_splits!=1:
+    #     try:
+    #         splits = split_training_dataset(X_train, n_splits)
+    #         X_train = splits[cid]
+    #     except:
+    #         print("Split data failed!")
+    #         exit()
 
     X_train = reshaping_data(X_train, timesteps=timesteps)
     X_test = reshaping_data(X_test, timesteps=timesteps)
