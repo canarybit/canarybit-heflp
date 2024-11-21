e95153629866277a5842ccc8e362e865799292cf
yijing <hejdesperado@gmail.com>
2024-11-21 18:29:58 +0100

    fix

--- src/fl-client-ti.py
+++ src/fl-client-ti.py
@@ -109,7 +109,7 @@ class LSTMRunner(TensorflowRunner):
 
     def train(self, model, epochs: int = 50):
 
-        train_gen = data_generator(batch_size=self.batch_size.batch_size, timesteps=self.timesteps, input_data=self.X_train, n_batches=self.n_batches)
+        train_gen = data_generator(batch_size=self.batch_size, timesteps=self.timesteps, input_data=self.X_train, n_batches=self.n_batches)
 
         early_stopping = EarlyStopping(monitor='loss', patience=10, verbose=1, restore_best_weights=True,
                                    min_delta=0.001, mode='min')
