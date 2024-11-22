a6baa9f2c5168eb6fe746104cbe85615c18cdd57
yijing <hejdesperado@gmail.com>
2024-11-22 08:59:17 +0100

    fix

--- heflp/heflp/client/basic.py
+++ heflp/heflp/client/basic.py
@@ -27,9 +27,4 @@ class BasicClient(fl.client.NumPyClient):
         print("mae loss", mae_loss, type(mae_loss))
         accuracy = 0.9
         print("accuracy", accuracy, type(accuracy))
-        return EvaluateRes(
-            Status(Code.OK, 'success'),
-            mae_loss,
-            size,
-            {"accuracy": accuracy}
-        )
+        return (mae_loss, size, {"accuracy": accuracy})
