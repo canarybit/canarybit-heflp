816eff60172d1c6adabc9c5c8c74e080a494d614
yijing <hejdesperado@gmail.com>
2024-12-03 02:07:17 +0100

    customisee training

--- src/fl-client-ti.py
+++ src/fl-client-ti.py
@@ -194,7 +194,7 @@ if __name__ == '__main__':
     if if_training:
         '''If standard training'''
         optimizer = keras.optimizers.Adam(learning_rate=0.001)
-        runner = LSTMRunner(X_train, X_test, X_test_reshaping, cols, 'mse', optimizer, 200)
+        runner = LSTMRunner(X_train, X_test, X_test_reshaping, cols, 'mse', optimizer, 50)
     else:
         '''If test only:'''
         train_gen = static_weight_generator(1000)
