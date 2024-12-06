cba145555a3d75eed1a255f55a9fe17ed9b836ff
yijing <hejdesperado@gmail.com>
2024-12-06 01:03:33 +0100

    use original training

--- src/fl-client-ti.py
+++ src/fl-client-ti.py
@@ -37,8 +37,8 @@ def load_data(cid:int=0, n_splits:int=1, timesteps:int=10):
     data_directory = "canarybit-heflp/data/ti_training" 
     input_data_folder_path=os.path.join(os.path.dirname(os.getcwd()), data_directory, str(cid))
 
-    X_train = reading_files(os.path.join(input_data_folder_path,"X_train_original.csv"))
-    X_test = reading_files(os.path.join(input_data_folder_path,"X_test_original.csv"))
+    X_train = reading_files(os.path.join(input_data_folder_path,"X_train_formatted.csv"))
+    X_test = reading_files(os.path.join(input_data_folder_path,"X_test_formatted.csv"))
     cols = list(X_test.columns) 
     # if n_splits!=1:
     #     try:
