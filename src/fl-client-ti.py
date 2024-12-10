01d96cbd46efab237babd36bdcb7bf95c8a114aa
yijing <hejdesperado@gmail.com>
2024-12-10 14:35:14 +0100

    use original training

--- src/fl-client-ti.py
+++ src/fl-client-ti.py
@@ -152,7 +152,6 @@ class LSTMRunner(TensorflowRunner):
         TN=len(X_test_reshaping[(X_test_reshaping["Attack"]=='0') & (X_test_reshaping['anomaly'] == False)])
         FP=len(X_test_reshaping[(X_test_reshaping["Attack"]=='0') & (X_test_reshaping['anomaly'] == True)])
         FN=len(X_test_reshaping[(X_test_reshaping["Attack"]=='1') & (X_test_reshaping['anomaly'] == False)])
-        FNrate=TP/(TP+FN)
 
         print("TP: ", TP, "TN: ", TN)
         print("FN: ", FN, "FP: ", FP)
