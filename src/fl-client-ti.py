ac835fac760f3d720ff39791e47d66d5450bfe2f
yijing <hejdesperado@gmail.com>
2024-12-16 04:58:35 +0100

    use original training

--- src/fl-client-ti.py
+++ src/fl-client-ti.py
@@ -37,6 +37,12 @@ def load_data(cid:int=0, total_n: int=2, timesteps:int=10):
     data_directory = "canarybit-heflp/data/10-17" 
     print("CLIENT CID", str(int(cid/2)), cid%2)
 
+    if total_n == 2:
+        input_data_folder_path=os.path.join(os.path.dirname(os.getcwd()), data_directory, str(cid))
+
+        X_train = reading_files(os.path.join(input_data_folder_path,"X_train.csv"))
+        X_test = reading_files(os.path.join(input_data_folder_path,"X_test.csv"))
+
     if total_n > 4:
         folder_num = 0 if cid == 0 else 1
         input_data_folder_path=os.path.join(os.path.dirname(os.getcwd()), data_directory, str(folder_num))
