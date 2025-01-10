c315dace1f0a18f0f04ba87104b4538589a57fd5
yijing <hejdesperado@gmail.com>
2025-01-10 01:28:23 +0100

    test

--- src/initialize_lstm_model.py
+++ src/initialize_lstm_model.py
@@ -134,8 +134,8 @@ X_train = reshaping_data(X_train, timesteps=timesteps,test=False)
 print("\nTraining data shape:", X_train.shape)
 
 input_shape = (X_train.shape[1], X_train.shape[2])
-lstm_autoencoder = create_lstm_autoencoder(input_shape, DEFAULT_MODEL_CONF)
-lstm_autoencoder.summary()
+# lstm_autoencoder = create_lstm_autoencoder(input_shape, DEFAULT_MODEL_CONF)
+# lstm_autoencoder.summary()
 
 initial_seq = Sequential()
 initial_seq.add(Input(shape=input_shape))
