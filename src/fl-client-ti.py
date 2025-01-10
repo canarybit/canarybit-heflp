f84996ed9ea39d4bd92e1b89e63b4f23a914907b
yijing <hejdesperado@gmail.com>
2025-01-10 01:52:14 +0100

    test

--- src/fl-client-ti.py
+++ src/fl-client-ti.py
@@ -241,8 +241,8 @@ if __name__ == '__main__':
     # Load the training data
     X_train, X_test, X_test_reshaping, cols = load_data(cid, total_n)
 
-    lstm_autoencoder = create_autoencoder(X_train.shape, DEFAULT_MODEL_CONF)
-    lstm_autoencoder.summary()
+    model = create_autoencoder(X_train.shape, DEFAULT_MODEL_CONF)
+    model.summary()
 
     # Define the runner
     # Here I also wrote a FakeRunner to support debugging without real training.
