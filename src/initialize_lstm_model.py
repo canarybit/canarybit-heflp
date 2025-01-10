936662fa3e4da5cd091031e67274409121a62bb6
yijing <hejdesperado@gmail.com>
2025-01-10 02:33:57 +0100

    test

--- src/initialize_lstm_model.py
+++ src/initialize_lstm_model.py
@@ -2,8 +2,9 @@ from models import FCN
 import numpy as np
 # from heflp.training.params import save_flattened_model_params
 from threatintellidataset import *
-from heflp.training.params import flatten_model_params, unflatten_model_params
-
+from heflp.training.params import flatten_model_params
+import keras
+import torch
 import sys
 import pandas as pd
 timesteps_max = 10
@@ -30,6 +31,30 @@ def timesteps_calculation(df, timesteps_max):
 #         raise ValueError("Invalid model type. Expecting PyTorch or Keras model.")
 #     return params
 
+def unflatten_model_params(flattened_params:NDArray, model):
+    '''Unflatten the 1D Vector and update the model parameters accordingly'''
+    if "torch" in sys.modules and isinstance(model, torch.nn.Module):
+        start = 0
+        for param in model.parameters():
+            end = start + param.numel()
+            param.data = torch.from_numpy(flattened_params[start:end].reshape(param.shape).astype(np.float32))
+            start = end
+    elif "keras" in sys.modules and isinstance(model, keras.Model):
+        unflattened_weights = []
+        start = 0
+        for param in model.get_weights():
+            shape = param.shape
+            print("Model shape:", shape)
+            print("Len of flattened_params:", len(flattened_params))
+            end = start + np.prod(shape)
+            unflattened_weight = np.array(flattened_params[start:end]).reshape(shape).astype(np.float32)
+            unflattened_weights.append(unflattened_weight)
+            start = end
+        model.set_weights(unflattened_weights)
+    else:
+        raise ValueError("Invalid model type. Expecting PyTorch or Keras model.")
+    return model
+
 def save_flattened_model_params(filepath:str, model):
     '''Save the model parameters as a 1D vector'''
     params = flatten_model_params(model)
@@ -48,5 +73,6 @@ lstm_autoencoder = create_autoencoder(X_train.shape, DEFAULT_MODEL_CONF)
 lstm_autoencoder.summary()
 
 save_flattened_model_params("lstm_init.npy", lstm_autoencoder)
-array = np.load('lstm_init.npy')
-print(array.shape)
+paras = np.load('lstm_init.npy')
+
+unflatten_model_params(paras[0], lstm_autoencoder)
