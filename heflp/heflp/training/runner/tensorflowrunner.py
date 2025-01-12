46f5632b9447e53e2031c0f9f3dc3938fcf30048
yijing <hejdesperado@gmail.com>
2025-01-12 20:32:15 +0100

    fix size

--- heflp/heflp/training/runner/tensorflowrunner.py
+++ heflp/heflp/training/runner/tensorflowrunner.py
@@ -46,7 +46,8 @@ class TensorflowRunner(Runner):
             raise RunnerException(f"Failed to compile the model: {e.args[0]}")
 
     def train(self, model: keras.Model, epochs: int = 1):
-        print("steps_per_epoch:", len(X_train)//self.batch_size)
+        print("Trining start:")
+        print("Train size:", len(X_train),"Batch size:", self.batch_size, "Per epoch:", len(X_train)//self.batch_size)
         self._compile_model(model)
 
         early_stopping = EarlyStopping(monitor='loss', patience=10, verbose=1, restore_best_weights=True,
