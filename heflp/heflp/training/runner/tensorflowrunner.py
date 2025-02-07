5293caa800fbde4550d7b73c3cc245ea5c1d9244
yijing <hejdesperado@gmail.com>
2025-02-07 05:11:15 +0100

    Fix cross entrophy

--- heflp/heflp/training/runner/tensorflowrunner.py
+++ heflp/heflp/training/runner/tensorflowrunner.py
@@ -64,7 +64,7 @@ class TensorflowRunner(Runner):
         try:
             if model._is_compiled and not force:
                 return
-            model.compile(optimizer=self.optimizer, loss=self.custom_binary_crossentropy, metrics=[self.metric])
+            model.compile(optimizer=self.optimizer, loss=self.criterion, metrics=[self.metric])
         except Exception as e:
             raise RunnerException(f"Failed to compile the model: {e.args[0]}")
 
